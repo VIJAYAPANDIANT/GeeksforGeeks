e4967da69b98c0ed1e8d67cf84ee8e596a1026aa
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-01-05 19:32:28 +0530

    vj

---
+++ String/Day22.java
@@ -0,0 +1,42 @@
+/*Fizz Buzz
+Fizz Buzz Problem involves that given an integer n, for every integer 0 < i <= n, the task is to output,
+
+"FizzBuzz" if i is divisible by 3 and 5,
+"Fizz" if i is divisible by 3,
+"Buzz" if i is divisible by 5
+"i" as a string, if none of the conditions are true.
+Return an array of strings.
+
+Examples :
+
+Input: n = 3
+Output: ["1", "2", "Fizz"]
+Explanation: 1 and 2 are neither divisible by 3 nor 5, so we just output 1 and 2, and 3 is divisible by 3 so we output "Fizz".
+Input: n = 10
+Output: ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+Input: n = 20
+Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”, “16”, “17”, “Fizz”, “19”, “Buzz”] */
+/*class Solution {
+    public static ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
+ */
+/*⏱️ Complexity
+
+Time: O(n)
+
+Space: O(n) */
